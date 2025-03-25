a83bdffc3ded4b3343e49a8ab39dc5dbca023461
DejanLat <PrecisionArcInnovations@gmail.com>
2025-03-25 18:49:21 -0400

    V0.2
    Pages are working now

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HeroSection from './components/HeroSection';
-import Cards from './components/Cards';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
-import Footer from './components/Footer';
-
+import SignUp from './components/pages/Contact';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <HeroSection />
-        <Cards />
-        <Routes>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+      <Footer /> {/* Moved Footer here so it appears on all pages */}
+    </Router>
   );
 }
 
